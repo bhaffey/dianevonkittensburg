2ed16dd9b54e06a41e37a593813dd8eba5921145
Bryn Haffey <bh81118n@pace.edu>
2014-04-15 21:20:28 -0400

    all
    all

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+  include ('item_sc_fns.php');
+  // The shopping cart needs sessions, so start one
+  session_start();
+  do_html_header("Diane Von Kittensburg");
+
+  echo "<p style=\"clear:both\">&nbsp;";
+  echo "<img src=\"images/stockphoto.jpg\" height=\"600px\"></img>";
+  echo "</p>";
+
+  //echo "<p>Please choose a category:</p>";
+
+  // get categories out of database
+  $category_array = get_categories();
+
+  // display as links to cat pages
+  display_categories($category_array);
+
+  // if logged in as admin, show add, delete, edit cat links
+  if(isset($_SESSION['admin_user'])) {
+    display_button("admin.php", "admin-menu", "Admin Menu");
+  }
+  do_html_footer();
+?>
---
+++ show_cat.php
@@ -0,0 +1,30 @@
+<?php
+  include ('item_sc_fns.php');
+  // The shopping cart needs sessions, so start one
+  session_start();
+
+  $catid = $_GET['catid'];
+  $name = get_category_name($catid);
+  do_html_header($name);
+
+  // get the item info out from db
+  //$items_array = get_categories($catid);
+  $items_array = get_items($catid);
+
+  display_items($items_array);
+
+//print array for testing purposes
+  print_r($items_array);
+
+  // if logged in as admin, show add, delete item links
+  if(isset($_SESSION['admin_user'])) {
+    display_button("index.php", "continue", "Continue Shopping");
+    display_button("admin.php", "admin-menu", "Admin Menu");
+    display_button("edit_category_form.php?catid=".$catid,
+                   "edit-category", "Edit Category");
+  } else {
+    display_button("index.php", "continue-shopping", "Continue Shopping");
+  }
+
+  do_html_footer();
+?>
