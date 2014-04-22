10cc4b410eb6f466ad147f7db1bbd1e6164a2c8b
Bryn Haffey <bh81118n@pace.edu>
2014-04-21 23:40:17 -0400

    alignment

--- login.php
+++ login.php
@@ -2,7 +2,11 @@
  require_once('item_sc_fns.php');
  do_html_header("Administration");
 
+  echo "<div style=\"margin-left: 680px; \">";
+
  display_login_form();
+
+  echo "</div>";
 
  do_html_footer();
 ?>
